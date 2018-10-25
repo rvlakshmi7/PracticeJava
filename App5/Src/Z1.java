aae606fc17aeb82cdd15a2d24e8b501743f5c646
Vijayalakshmi <r.vlakshmi7@gmail.com>
2018-10-25 15:13:11 +0530

    Second again

---
+++ App5/Src/Z1.java
@@ -0,0 +1,24 @@
+class Z 
+{
+	static double test(boolean b,int i,char j,int k)
+	{
+		System.out.println("frm test");
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(b);
+		return 200.44;
+
+	}
+	public static void main(String[] args) 
+	{
+
+		System.out.println("from main");
+			double d1 =test(false, 20, 'b', 30);
+			
+
+
+		System.out.println("end of main" +d1);
+		System.out.println(test(false, 20, 'b', 30));
+	}
+}
